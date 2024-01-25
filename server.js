bcbd00398461aa1050cfb0065d3b0a35dd9bdd47
Glitch (glitch-hello-node) <none>
2024-01-25 18:01:47 +0000

    💖🕌 Checkpoint ./server.js:63416232/6910

--- server.js
+++ server.js
@@ -1,144 +1,101 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+// server.js
+// where your node app starts
 
-// const express = require("express");
-// const app = express();
-
-// app.use(express.json());
-
-// app.get("api/accounts", (req, resp) => {
-//   resp.json()
-// });
+// init project
+const express = require("express");
+const app = express();
 
 
+app.use(express.json());
 
-// -----------
 
-const path = require("path");
+let checkingAccount = new Account('Mara', 'checking', 300);
+checkingAccount.id = 'c78987bd-c0de-4691-a6de-24399459e911'
 
-// const express = require("express");
-// const app = express();
+let savingAccount = new Account('Mara', 'saving', 200);
+savingAccount.id = '049acbea-cc59-4ef7-8cc6-7213971405ca';
 
-// app.use(express.json());
+// let checkingAccount = {
+//   name: "Mara",
+//   id: "629dcdce-cc95-4328-8f26-bcab03f69a02",
+//   accountType: "checking",
+//   balance: 200,
+// };
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
+// let savingAccount = {
+//   name: "Mara",
+//   id: "c78987bd-c0de-4691-a6de-24399459e911",
+//   accountType: "saving",
+//   balance: 500,
+// };
+
+module.exports.accounts = [checkingAccount, savingAccount];
+let accounts = module.exports.accounts;
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
+app.get("/dreams", function (request, response) {
+  response.json(dreams);
 });
 
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
-
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
+app.get("/books", function (request, response) {
+  response.sendFile(__dirname + "/response1.json");
 });
 
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
+app.get("/api/accounts", (req, resp) => {
+  resp.json(accounts);
 });
 
-fastify.get("/api/accounts", function (req, resp) {
-            return resp.view("/src/pages/index.hbs")
-            })
-// app.get("api/accounts", (req, resp) => {
-//   resp.json()
-// });
+app.post("/api/accounts", (req, resp) => {
+  let name = req.body.name;
+  let accountType = req.body.accountType;
+  let balance = req.body.balance;
 
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
+  // let account = { name: name, accountType: accountType, balance: balance };
+  let account = new Account(name, accountType, balance)
 
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
+//   accounts.push(account);
 
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
+//   resp.json({ success: true });
+  
+  account.save((newAccount, error) => {
+    if(newAccount) {
+      accounts.push(newAccount);
+      resp.json({success: true});
     } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
+      resp.json({success: false, error, error});
     }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
+  });
 });
 
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
+app.post("/api/transfer", (req, resp) => {
+  
+  console.log(req.body);
+  
+  let accountFromId = req.body.accountFromId;
+  let accountToId = req.body.accountToId;
+  let amount = req.body.amount;
+  
+  let fromAccount = accounts.find(account => account.id == accountFromId);
+  let toAccount = accounts.find(account => account.id == accountToId);
+  
+  fromAccount.transfer(toAccount, amount, (transferred, error) => {
+    if(transferred) {
+      resp.json({success:true});
+    } else {
+      resp.json({success: false, error: error});
     }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
+  });
+  
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
