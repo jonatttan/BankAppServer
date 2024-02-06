a2115c1a3f8867cb77bed0d5a12c634503c07fff
Glitch (glitch-hello-node) <none>
2024-02-06 00:49:34 +0000

    🐛🏞 Checkpoint ./acount.js:63416232/146 ./server.js:63416232/473

--- account.js
+++ account.js
@@ -1,13 +1,20 @@
 let app = require("./server.js")
 let uuid = require("./uuidv4.js")
 
-class Acount {
+class Account {
   
   constructor(name, accountType, balance) {
     this.id = ""
     this.name = name
     this.accountType = accountType
-    
+    this.balance = balance
+  }
+  
+  save(completion) {
+    this.id = uuid()
+    completion(this)
   }
   
 }
+
+module.exports = Account;
--- server.js
+++ server.js
@@ -4,9 +4,16 @@
 // init project
 const express = require("express");
 const app = express();
+const Account = require("./account");
 
 app.use(express.json());
 app.use(express.static("public"));
+
+let sampleCheckingAccount = new Account('Adao', 'checking', 22);
+let sampleSavinggAccount = new Account('Eva', 'saving', 33);
+
+module.exports.accounts = [sampleCheckingAccount, sampleSavinggAccount];
+let accounts = module.exports.accounts;
 
 app.get("/api/accounts", (req, resp) => {
   resp.json("test");
