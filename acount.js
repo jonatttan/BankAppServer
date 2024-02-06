f2e6576c0d8db01a7d6de4ee5b39f6ca80561d65
Glitch (glitch-hello-node) <none>
2024-02-06 00:37:13 +0000

    🎁📒 Checkpoint ./acount.js:63416232/241 ./uuidv4.js:63416232/301

--- acount.js
+++ acount.js
@@ -1,1 +1,13 @@
-let app = require()
+let app = require("./server.js")
+let uuid = require("./uuidv4.js")
+
+class Acount {
+  
+  constructor(name, accountType, balance) {
+    this.id = ""
+    this.name = name
+    this.accountType = accountType
+    
+  }
+  
+}
